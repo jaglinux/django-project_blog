3c34670146cf9292a2f6b6aa946d40f076c5aef1
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-10-01 11:25:47 -0700

    10/1: Add threading prints
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("room/", views.room, name="room"),
+    path("test_thread/", views.test_thread, name="test_thread"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import threading
+import time
 
 # Create your views here.
 
 def home(request):
+    print("home request is ", request)
+    print("home request thread is ", threading.get_native_id())
     return HttpResponse("Jag home page")
 
 def room(request):
+    print("room request is ", request)
+    print("room request thread is ", threading.get_native_id())
     return HttpResponse("Jag Room")
+
+def test_thread(request):
+    print("test_thread request is ", request)
+    print("thread sleep start ", threading.get_native_id())
+    time.sleep(20)
+    print("thread sleep end ", threading.get_native_id())
+    return HttpResponse("Test_thread")
