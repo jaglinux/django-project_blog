9a3d51c11a2ba0ecf5b832ff6d9bbfec072826b8
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-10-15 12:59:36 -0700

    Pytorch: Add dynamic url, model, migration, admin panel
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- pytorch/urls.py
+++ pytorch/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.pytorch, name="pytorch"),
+    path("<str:pk>/", views.pytorch_job, name="pytorch_job"),
 ]
--- pytorch/views.py
+++ pytorch/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import PytorchJob
 
 # Create your views here.
 def pytorch(request):
+    pytorchJob = PytorchJob.objects.create(job_out = "Running job ")
+    pytorchJob.save()
     return HttpResponse("Pytorch home page")
+
+def pytorch_job(request, pk):
+    return HttpResponse(f"Pytorch job page {pk}")
